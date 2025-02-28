60ff8fee4aa86f75582d72550f8dcc6ea94dcc77
Andrey Underoak <eaa4robo@gmail.com>
2025-02-28 18:36:43 +0300

    working with led

--- lab2/Src/main.c
+++ lab2/Src/main.c
@@ -1,15 +1,17 @@
 #define STM32F446xx
 #include "stm32f4xx.h"
 
-// #include <stdio.h>
+
 
 volatile uint8_t received_bytes[] = {0,0};
 volatile uint8_t transmit_bytes[] = {0,0};
 volatile uint8_t byte_index = 0;
-volatile uint8_t led_status[] = {0,0,0,0,0,0,0,0};
+volatile uint8_t led_status[] = {0,0,0,0,0,0,0,0,0};
 
 
 /*
+   1й LED = 1й LED
+
     0-8 зажечь от 0 до 8 светодиодов
     10 + 1-8 вернуть информацию о светодиоде
     20 + 2 ping
@@ -27,24 +29,16 @@ uint8_t lab_proto(uint8_t message);
 void led_switch(uint8_t id, uint8_t status);
 
 void USART2_IRQHandler(void) {
-    // receive
-    // if (USART2->SR & USART_SR_RXNE) { 
-    //     received_byte = USART2->DR; 
-    //     received_byte = received_byte - '0';
-    //     transmit_byte = '0' + lab_proto(received_byte); 
-    //     USART2->DR = transmit_byte;   
-    //     // Ожидание отправки
-    //     while (!(USART2->SR & USART_SR_TXE)); 
-    // }
 
-    if (USART2->SR & USART_SR_RXNE) {  // Прерывание по приему
-        received_bytes[byte_index] = USART2->DR;   // Читаем принятый байт
+
+    if (USART2->SR & USART_SR_RXNE) { 
+        received_bytes[byte_index] = USART2->DR;  
         byte_index++;
         
-        if (byte_index >= 2) {  // Если приняли 2 байта
+        if (byte_index >= 2) {  
             
             uint8_t message = (received_bytes[0] - '0') * 10 + (received_bytes[1] - '0');
-
+            
             uint8_t out = lab_proto(message);
 
             transmit_bytes[0] = out / 10 + '0';
@@ -53,7 +47,7 @@ void USART2_IRQHandler(void) {
 
             for (uint8_t i = 0; i < 2; ++i) {
                 USART2->DR = transmit_bytes[i];
-                while (!(USART2->SR & USART_SR_TXE)); // Ждем, пока буфер передатчика освободится
+                while (!(USART2->SR & USART_SR_TXE)); 
                 }
         }
     }
@@ -82,8 +76,25 @@ void UART2_Init(void) {
     NVIC_EnableIRQ(USART2_IRQn); 
 }
 
+void init_GPIO() {
+    // тактирование портов C и D
+    RCC->AHB1ENR |= (1 << 2) | (1 << 3);
+    
+    // PC4 - PC11 выходы
+    for (int i = 4; i <= 11; i++) {
+        GPIOC->MODER &= ~(3 << (i * 2));
+        GPIOC->MODER |= (1 << (i * 2));
+    }
+    
+    // PC13 и PD2 входы
+    GPIOC->MODER &= ~(3 << (13 * 2));
+    GPIOD->MODER &= ~(3 << (2 * 2));
+    
+}
+
 int main(void) {
     UART2_Init();
+    init_GPIO();
     for (;;) {
     }
 }
@@ -92,14 +103,15 @@ int main(void) {
 uint8_t lab_proto(uint8_t message){
     uint8_t ret_val = message;
     if(message < 9){
-        uint8_t i = 0;
-        for(i; i < message; ++i)
+        uint8_t i = 1;
+        for(i; i <= message; ++i)
             led_switch(i, 1);
         for(i; i < 9; ++i)
             led_switch(i, 0);
     }
-    else if(message > 10 && message < 19)
+    else if(message > 10 && message < 19){
         ret_val = led_status[message - 10];
+    }   
     else{
         switch (message)
         {
@@ -112,7 +124,7 @@ uint8_t lab_proto(uint8_t message){
                 led_switch(i, 1);
             break;
         case 24:
-            for(uint8_t i = 0; i < message; ++i)
+            for(uint8_t i = 1; i < message; ++i)
                 led_switch(i, 0); 
             break;
         }
@@ -122,4 +134,6 @@ uint8_t lab_proto(uint8_t message){
 
 void led_switch(uint8_t id, uint8_t status){
     led_status[id] = status;
+    GPIOC->ODR &= ~(1 << (3+id));
+    GPIOC->ODR |= status << (3+id);
 }
