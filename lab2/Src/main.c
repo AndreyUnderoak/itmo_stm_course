948002bd88039d919707a4cec476adbed1b28fe7
Andrey Underoak <eaa4robo@gmail.com>
2025-03-03 18:15:39 +0300

    done

--- lab2/Src/main.c
+++ lab2/Src/main.c
@@ -2,12 +2,18 @@
 #include "stm32f4xx.h"
 
 
+#define STM32F446xx
+#include "stm32f4xx.h"
+
+#define USART2_BAUDRATE 9600
+#define PCLK1_FREQ 16000000
+#define USART2_BRR (PCLK1_FREQ / USART2_BAUDRATE)
 
 volatile uint8_t received_bytes[] = {0,0};
 volatile uint8_t transmit_bytes[] = {0,0};
-volatile uint8_t byte_index = 0;
 volatile uint8_t led_status[] = {0,0,0,0,0,0,0,0,0};
-
+volatile uint8_t received_count = 0;
+volatile uint8_t data_ready = 0;
 
 /*
    1й LED = 1й LED
@@ -26,32 +32,35 @@ uint8_t lab_proto(uint8_t message);
     id 1-8
     status 0/1
 */
-void led_switch(uint8_t id, uint8_t status);
+uint8_t check_led_status(uint8_t id);
 
 void USART2_IRQHandler(void) {
-
-
-    if (USART2->SR & USART_SR_RXNE) { 
-        received_bytes[byte_index] = USART2->DR;  
-        byte_index++;
-        
-        if (byte_index >= 2) {  
-            
+    if (USART2->SR & USART_SR_RXNE) {
+        received_bytes[received_count++] = USART2->DR; 
+        if (received_count == 2) {
             uint8_t message = (received_bytes[0] - '0') * 10 + (received_bytes[1] - '0');
-            
             uint8_t out = lab_proto(message);
-
             transmit_bytes[0] = out / 10 + '0';
             transmit_bytes[1] = out % 10 + '0';
-            byte_index = 0;
-
-            for (uint8_t i = 0; i < 2; ++i) {
-                USART2->DR = transmit_bytes[i];
-                while (!(USART2->SR & USART_SR_TXE)); 
-                }
+            
+            data_ready = 1;
+            received_count = 0;
+            USART2->CR1 |= USART_CR1_TXEIE; 
+        }
+    }
+    
+    if ((USART2->SR & USART_SR_TXE) && data_ready==1) {
+        static uint8_t transmit_count = 0;
+        uint8_t buf = transmit_bytes[transmit_count++];
+        USART2->DR = buf; 
+        if (transmit_count == 2) {
+            transmit_count = 0;
+            data_ready=0;
+            USART2->CR1 &= ~USART_CR1_TXEIE;
         }
     }
 }
+
 
 
 void UART2_Init(void) {
@@ -110,7 +119,7 @@ uint8_t lab_proto(uint8_t message){
             led_switch(i, 0);
     }
     else if(message > 10 && message < 19){
-        ret_val = led_status[message - 10];
+        ret_val = check_led_status(message - 10);
     }   
     else{
         switch (message)
@@ -133,7 +142,17 @@ uint8_t lab_proto(uint8_t message){
 }
 
 void led_switch(uint8_t id, uint8_t status){
-    led_status[id] = status;
+    // led_status[id] = status;
     GPIOC->ODR &= ~(1 << (3+id));
     GPIOC->ODR |= status << (3+id);
+}
+
+uint8_t check_led_status(uint8_t id) {
+    if (id < 1 || id > 8) {
+        return 0; 
+    }
+
+    uint8_t pin = 3 + id; 
+
+    return (GPIOC->IDR & (1 << pin)) ? 1 : 0;  
 }
