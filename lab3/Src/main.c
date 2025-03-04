111f76ce9a53175ca4155cccfc649ec921c1ffb0
Andrey Underoak <eaa4robo@gmail.com>
2025-03-04 13:33:59 +0300

    add third

--- lab3/Src/main.c
+++ lab3/Src/main.c
@@ -1,78 +1,86 @@
 #define STM32F446xx
 #include "stm32f4xx.h"
 
-#define LED_PIN 5  
-#define TIMER_FREQUENCY_HZ 1000  
+#define LED_PIN 5 
+// частота прерывания
+#define TIMER_FREQUENCY_HZ 5000
 
-void SystemClock_Config(void);
-void Timer_Config(void);
-void GPIO_Config(void);
+void SystemClock_INIT(void);
+void Timer_INIT(void);
+void GPIO_INIT(void);
 
 int main(void) {
-    SystemClock_Config(); 
-    GPIO_Config();        
-    Timer_Config();      
+    SystemClock_INIT(); 
+    GPIO_INIT();       
+    Timer_INIT();      
 
     while (1) {
-        
     }
 }
 
-void SystemClock_Config(void) {
-    // Отключаем PLL перед настройкой
+void SystemClock_INIT(void) {
+    // PLL off
     RCC->CR &= ~RCC_CR_PLLON;
     while (RCC->CR & RCC_CR_PLLRDY);
 
-    // PLL (HSE = 8 MHz, желаемая SYSCLK = 180 MHz)
-    // f(VCO clock) = f(HSE) / M * N = 8MHz / 4 * 180 = 360 MHz
-    // f(PLL output) = f(VCO clock) / P = 360 MHz / 2 = 180 MHz
-    RCC->PLLCFGR = (4 << RCC_PLLCFGR_PLLM_Pos) |  // M = 4
-                    (180 << RCC_PLLCFGR_PLLN_Pos) | // N = 180
-                    (0 << RCC_PLLCFGR_PLLP_Pos) |  // P = 2
-                    (RCC_PLLCFGR_PLLSRC_HSE);     // HSE
+    // PLL (HSE = 8 MHz, SYSCLK = 150 MHz)
+    // f(VCO clock) = f(HSE) / M * N = 8MHz / 4 * 150 = 300 MHz
+    // f(PLL output) = f(VCO clock) / P = 300 MHz / 2 = 150 MHz
+    RCC->PLLCFGR = (4 << RCC_PLLCFGR_PLLM_Pos) |    // M = 4
+                    (150 << RCC_PLLCFGR_PLLN_Pos) | // N = 150
+                    (0 << RCC_PLLCFGR_PLLP_Pos) |   // P = 2
+                    (RCC_PLLCFGR_PLLSRC_HSE);       // HSE
     
-    //  HSE on
+    // HSE on
     RCC->CR |= RCC_CR_HSEON;
     while (!(RCC->CR & RCC_CR_HSERDY));
     
-    // PLL
+    // PLL on
     RCC->CR |= RCC_CR_PLLON;
     while (!(RCC->CR & RCC_CR_PLLRDY));
     
-    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;  // AHB 180 MHz
-    RCC->CFGR |= RCC_CFGR_PPRE1_DIV4; // APB1 45 MHz
-    RCC->CFGR |= RCC_CFGR_PPRE2_DIV2; // APB2 90 MHz
+    // AHB 150 MHz
+    RCC->CFGR |= RCC_CFGR_HPRE_DIV1; 
+    // APB1 75 / 2 MHz 
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+    // APB2 75 MHz 
+    RCC->CFGR |= RCC_CFGR_PPRE2_DIV2; 
     
-    // Выбор PLL 
+    // PLL source
     RCC->CFGR |= RCC_CFGR_SW_PLL;
     while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
 }
 
-void GPIO_Config(void) {
-
+void GPIO_INIT(void) {
+    // GPIOA tac
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
- 
+    
+    // PA5 out
     GPIOA->MODER |= (1 << (LED_PIN * 2));
     GPIOA->OTYPER &= ~(1 << LED_PIN);
     GPIOA->OSPEEDR |= (3 << (LED_PIN * 2));
 }
 
-void Timer_Config(void) {
-    //  TIM2 tac
+void Timer_INIT(void) {
+    // tim2 tac
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
     
-    TIM2->PSC = (45000 - 1); // (APB1 = 45MHz, 45MHz / 45000 = 1kHz)
-    TIM2->ARR = (1000 - 1);  //(1kHz / 1000 = 1Hz)
+    // Предделитель (APB1 = 37.5MHz, 37.5MHz / 37500 = 1kHz)
+    TIM2->PSC = (37500 - 1); 
+    // reload (1kHz / 5 = 5kHz)
+    TIM2->ARR = (5 - 1); 
     
-    TIM2->DIER |= TIM_DIER_UIE; // прерывание по переполнению
-    TIM2->CR1 |= TIM_CR1_CEN;   // Включить таймер
+    // прерывание по переполнению
+    TIM2->DIER |= TIM_DIER_UIE; 
+    // tim2 on
+    TIM2->CR1 |= TIM_CR1_CEN; 
     
-    NVIC_EnableIRQ(TIM2_IRQn);  
+    NVIC_EnableIRQ(TIM2_IRQn); 
 }
 
 void TIM2_IRQHandler(void) {
-    if (TIM2->SR & TIM_SR_UIF) { // Проверяем флаг обновления
-        TIM2->SR &= ~TIM_SR_UIF; // Сбрасываем флаг
-        GPIOA->ODR ^= (1 << LED_PIN); // Инвертируем состояние пина
+    if (TIM2->SR & TIM_SR_UIF) {
+        TIM2->SR &= ~TIM_SR_UIF; 
+        GPIOA->ODR ^= (1 << LED_PIN);
     }
 }
